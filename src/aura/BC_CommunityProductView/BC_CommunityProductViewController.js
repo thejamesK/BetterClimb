03cb9f9c873324197070a018a12ba8cbbfd38374
Jakub <jakub.krawczyk@britenet.com.pl>
2020-03-25 10:35:19 +0100

    updated product view

--- src/aura/BC_CommunityProductView/BC_CommunityProductViewController.js
+++ src/aura/BC_CommunityProductView/BC_CommunityProductViewController.js
@@ -9,6 +9,20 @@
             console.log('ids inside ->>>>>> ' + product);
             component.set('v.productWrapper', product);
             sessionStorage.removeItem('BC_CommunityProductView--product');
+            let productId = component.get('v.productWrapper.productId');
+            console.log(productId);
+            helper.loadRating(component, productId);
         }
-    }
+    },
+    onProductReviewAdded : function(component, event, helper) {
+        let reviews = component.find("reviews");
+        if (reviews) {
+            reviews.refresh();
+        }
+
+        component.find("tabs").set('v.selectedTabId', 'productReviewTab');
+        let productId = component.get('v.productWrapper.productId');
+        console.log('PRODUCT ID = ' + productId);
+        helper.loadRating(component, productId);
+    },
 })
