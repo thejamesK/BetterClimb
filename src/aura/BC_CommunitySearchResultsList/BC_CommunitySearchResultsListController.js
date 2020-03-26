096627bbe541964b3b40976385b7fc237b7098d5
Jakub <jakub.krawczyk@britenet.com.pl>
2020-03-26 09:52:57 +0100

    added new button

---
+++ src/aura/BC_CommunitySearchResultsList/BC_CommunitySearchResultsListController.js
@@ -0,0 +1,48 @@
+({
+    init : function (component, event, helper) {
+        console.log(sessionStorage.getItem('BC_CommunitySearchBar--recordIds'));
+        let idsJson = sessionStorage.getItem('BC_CommunitySearchBar--recordIds');
+        if(!$A.util.isUndefinedOrNull(idsJson)) {
+            let ids = JSON.parse(idsJson);
+            component.set('v.recordIds', ids);
+            sessionStorage.removeItem('BC_CommunitySearchBar--recordIds');
+        }
+    },
+    openModal: function(component, event, helper) {
+        // Set isModalOpen attribute to true
+        component.set("v.isModalOpen", true);
+    },
+    closeModal: function(component, event, helper) {
+        // Set isModalOpen attribute to false
+        component.set("v.isModalOpen", false);
+    },
+    handleLoad: function(cmp, event, helper) {
+        // cmp.set('v.showSpinner', false);
+    },
+    handleSubmit: function(cmp, event, helper) {
+        cmp.set('v.disabled', true);
+        // cmp.set('v.showSpinner', true);
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            title : 'New Product Added',
+            message: 'New Product successfully created.',
+            duration:' 4000',
+            key: 'info_alt',
+            type: 'success',
+            mode: 'pester'
+        });
+        toastEvent.fire();
+    },
+    handleError: function(cmp, event, helper) {
+        // errors are handled by lightning:inputField and lightning:messages
+        // so this just hides the spinner
+        // cmp.set('v.showSpinner', false);
+    },
+    handleSuccess: function(cmp, event, helper) {
+        var params = event.getParams();
+        cmp.set('v.recordId', params.response.id);
+        // cmp.set('v.showSpinner', false);
+        cmp.set('v.saved', true);
+        cmp.set('v.isModalOpen', false);
+    },
+})
